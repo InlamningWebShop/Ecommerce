753e34debf8708c701d8a6fc1f07ea94fb95e259
PayamS <72343921+Payam-Shaker@users.noreply.github.com>
2021-11-16 13:50:25 +0100

    category, product & stock models added.

---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecom.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public DateTime DateAdded { get; set; }
+        public decimal ListPrice { get; set; }
+        //[ForeignKey("Category")]
+        public Category Category { get; set; }
+        public int CategoryID { get; set; }
+        public Stock Stocks { get; set; }
+
+    }
+}
---
+++ Models/Stock.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecom.Models
+{
+    public class Stock
+    {
+        public Product Product { get; set; }
+        [Key]
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+
+
+    }
+}
